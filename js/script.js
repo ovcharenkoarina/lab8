c227dfede6bfd9fa67c1f7bda9a4a7d26b85f2ad
ovcharenkoarina <aovcharenko@hawk.iit.edu>
2023-11-06 18:24:51 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+  var button = document.getElementById('entrybutton');
 
+  function handleButtonClick(){
+    var inputValue = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = inputValue.value;
+    alert("Arina Ovcharenko: " + inputValue.value);
+  }
 
-
-
-
-
-
+  button.addEventListener('click', handleButtonClick);
+  }
+  
 window.addEventListener('load', init);
